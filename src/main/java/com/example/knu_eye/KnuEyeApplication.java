0c22367fd70b3aaa7f939299e5589151417f6608
Kimhyunggue <hyo06069@naver.com>
2026-05-07 12:50:36 +0900

    init: proj initialization

---
+++ src/main/java/com/example/knu_eye/KnuEyeApplication.java
@@ -0,0 +1,13 @@
+package com.example.knu_eye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnuEyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnuEyeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/knu_eye/KnuEyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.knu_eye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnuEyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
